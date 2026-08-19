b58d62470f4dbcf1584febe701b7be565236fbde
Bablumahto <bablumht18@gmail.com>
2026-08-19 17:49:26 +0530

    login-contorller

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,8 @@
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// register
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -25,4 +28,61 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+//login
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check required fields
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    // Send response
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Login failed",
+    });
+  }
+};
+
+module.exports = { registerUser, login };
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controller/user");
+const { registerUser, login } = require("../controller/user");
 router.post("/register", registerUser);
-
+router.post("/login", login);
 module.exports = router;
